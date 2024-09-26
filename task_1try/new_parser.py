20700b20cab2cedd289947e82eb809692605c7f6
Igor <kasusplug@gmail.com>
2024-09-26 22:51:21 +0300

    logs counter

--- task_1try/new_parser.py
+++ task_1try/new_parser.py
@@ -1,4 +1,3 @@
-
 class Logs:
 
     def __init__(self, file_name_read, file_name_record):
@@ -8,35 +7,46 @@ class Logs:
         self.logs_time_count_error = {}
         self.logs_time_count_debug = {}
 
-
     def logs_find(self):
         with open(self.file_name_read, 'r', encoding='utf8') as logs_events:
             with open(self.file_name_record, 'w', encoding='utf8') as new_logs:        
                 for line in logs_events:
-                    if line.strip().find("[INFO]"):
-                        timestamp = line[1:47]  
+                    line = line.strip()
+                    if "[INFO]" in line:
+                        timestamp = line[0:90] 
                         if timestamp in self.logs_time_count_info:
                             self.logs_time_count_info[timestamp] += 1  
                         else:
                             self.logs_time_count_info[timestamp] = 1  
-                        new_logs.write(line[1:47] + '\n')
-                    if line.strip().find("[ERROR]"):
-                        timestamp = line[1:47]
+                        new_logs.write(line + '\n')
+                    elif "[ERROR]" in line:
+                        timestamp = line[0:90]
                         if timestamp in self.logs_time_count_error:
                             self.logs_time_count_error[timestamp] += 1  
                         else:
                             self.logs_time_count_error[timestamp] = 1  
-                        new_logs.write(line[1:47] + '\n')
-                    if line.strip().find("[DEBUG]"):
-                        timestamp = line[1:47]
+                        new_logs.write(line + '\n')
+                    elif "[DEBUG]" in line:
+                        timestamp = line[0:90]
                         if timestamp in self.logs_time_count_debug:
                             self.logs_time_count_debug[timestamp] += 1  
                         else:
                             self.logs_time_count_debug[timestamp] = 1  
-                        new_logs.write(line[1:47] + '\n')
-                print('Succesfully get all logs')
-
+                        new_logs.write(line + '\n')
+                print('Successfully processed all logs')
+    
+    def logs_counter(self):
+        with open(r'task_1try\cathed_logs.log', 'w', encoding='utf8') as new_logs:
+            for timestamp, count in self.logs_time_count_info.items():
+                new_logs.write(f"  {timestamp} - {count}" + '\n')
+            for timestamp, count in self.logs_time_count_error.items():
+                new_logs.write(f"  {timestamp} - {count}" + '\n')
+            for timestamp, count in self.logs_time_count_debug.items():
+                new_logs.write(f"  {timestamp} - {count}" + '\n')
+            print('Succesfully counted ALL logs')
 
 check_logs = Logs(file_name_read=r'task_1try\logs.log',
                    file_name_record=r'task_1try\cathed_logs.log')
 check_logs.logs_find()
+check_logs.logs_counter()
+
